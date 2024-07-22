fe3fd72424c42418fcb9ccaf7bad29e779f4f21f
tantu1905 <hanifitan.ilhan@std.yeditepe.edu.tr>
2024-07-23 02:22:58 +0300

    new updates July 23 1.50

--- databases/database.py
+++ databases/database.py
@@ -10,7 +10,7 @@ conn_str = f"mssql+pyodbc:///?autocommit=true&odbc_connect={params}"
 
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(conn_str, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
